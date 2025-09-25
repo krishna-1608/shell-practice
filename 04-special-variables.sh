bf317763626a379af77ed1b71c783028dd8bd9c1
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-26 00:11:39 +0530

    spec-var

--- 04-special-variables.sh
+++ 04-special-variables.sh
@@ -2,9 +2,11 @@
 
 echo "all variables passed to scripts: $@"  #separtate string
 echo "all variables passed to scripts: $*" #single string 
-echo "script name : $0"
+echo "script name : $0" 
 echo "current dir : $PWD"
 echo "who is running : $USER"
 echo "home dire : $HOME"
-echo ""PID os this script : $$
+echo "PID os this script : $$"
+sleep 50 &
+echo "PID os the last command background process is :$!"
 echo "script status: $?"
