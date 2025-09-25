e882fb3d96a433fe97f6435844f65194337efb49
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-26 00:51:54 +0530

    spec-var

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#everything in shell treats as a string
 NUMBER1=120
 NUMBER2=100
 
-SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2)) 
 
 echo " SUM is : $SUM "
+
+#array 
+
+FRUITS=("apple" "banana" "grapes")
+
+echo "all fruits : ${FRUITS[@]}"
+echo "first fruit : ${FRUITS[0]}"
