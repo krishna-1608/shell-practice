05142b62d2e2317f201faeff32ac6d17d8fc02ec
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-26 17:29:59 +0530

    installation

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -4,6 +4,7 @@ echo "enter the number"
 read NUMBER
 
 if [$(($NUMBER%2)) -eq 0]; then
-  echo "given number $NUMBER is  even'
+  echo "given number $NUMBER is  even"
 else 
   echo "given number $NUMBER is odd"
+fi
