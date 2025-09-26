db0ac6fdc6da3ac808b972a8eded3a9f67fc8e57
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-26 17:42:47 +0530

    installation

--- 09-installiatoin.sh
+++ 09-installiatoin.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 userid=$(id -u)
 
-if [$userid -nq 0 ]; then
+if [$userid -ne 0 ]; then
     echo "error:: run the script with root access" # if it is failed stop script excution
     exit 1 #failuers is other than 0 
 fi
 
 VALIDATE(){   #functions recive inputs through arhs jut like shell script arguments  
-    if [$1 -nq 0]; then
+    if [$1 -ne 0]; then
         echo "error:: installing $2 is failuer"
         exit 1 # if it is failed stop script excution
     else
