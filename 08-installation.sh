8d0d15047dad06f7ac6a6dc5e149a76361ca818f
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-26 17:47:10 +0530

    installaition

--- 08-installation.sh
+++ 08-installation.sh
@@ -2,14 +2,14 @@
 
 userid=$(id -u)
 
-if [$userid -ne 0 ]; then
-    echo "error:: run the script with root access" # if it is failed stop script excution
+if [ $userid -ne 0 ]; then
+    echo "error:: run the script with root access" #if it is failed stop script excution
     exit 1 #failuers is other than 0 
 fi
 
 dnf install mysql -y
 
-if [$? -ne 0]; then
+if [ $? -ne 0]; then
     echo "error:: installing mysql is failuer"
     exit 1 # if it is failed stop script excution
 else
