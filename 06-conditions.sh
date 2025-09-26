177bcd76e277740c5b7ebe6ae7ffd5ee8ae3cb9d
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-26 17:24:13 +0530

    installation

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ]; then
+if [ $NUMBER -lt 10 ];then
    echo "given number is $NUMBER is less then 10"
 else
    echo "given number is $NUMBER is greater than 10 "
