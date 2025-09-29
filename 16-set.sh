72e3ef9cb63bf243b19754751844b37b21f236fc
krishna-1608 <krishnachaitanya.sugrivu@gmail.com>
2025-09-29 19:24:07 +0530

    Practice

--- 16-set.sh
+++ 16-set.sh
@@ -3,9 +3,6 @@
 # Exit if any command in a pipeline fails
 set -o pipefail
 
-touch file2
 # This pipeline will fail and cause the script to exit
 # The `ls` command will fail but `grep` will still run without this option
 ls /does_not_exist | grep "something"
-
-touch file1
